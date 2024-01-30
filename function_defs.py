210aea3382186565338f2d59bf722d13c529f258
Adrian Cavins <cavinswx@gmail.com>
2024-01-30 15:52:51 -0500

    started. realized i didn't know the qualifications for types of SALLJ. gave up.

--- function_defs.py
+++ function_defs.py
@@ -4,4 +4,18 @@ import metpy
 import scipy
 import xarray as xr
 
+def jet_occurrence_Oliviera(wind_u, wind_v):
+    if wind_u>3:
+        return True
+    elif len(np.where(abs(wind_v) > abs(wind_u))) > 0:
+        return False
+    else:
+        return False
+    
+def jet_occurence_Sasaki(wind_u, wind_v):
+    if wind_u>3:
+        return True
+    else:
+        return False
+
 
